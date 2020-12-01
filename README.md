de5f35fa551a94a9c4474d8e79cc5cc61282d4fe
Ben Mulhern <ben@benmulhern.co.uk>
2020-12-01 20:54:50 +0000

    Day 1 part 2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # advent-of-code
+
+Just run `ts-node src/day1.ts` for example.
--- src/day1.ts
+++ src/day1.ts
@@ -2,14 +2,17 @@ import * as fs from "fs"
 
 const text: string = fs.readFileSync(__dirname + "/day1_numbers.txt", "utf-8")
 const ints: number[] = text.split("\n").map((t) => Number(t))
-
 const target: number = 2020
-
-// Flatmap ints into a map of ints - result needs to be a, b pairs
-// Then find the pair that sums and return that
 const pairs = ints.flatMap((i) => ints.map((j) => [i, j]))
-console.log(pairs)
 const pair = pairs.find((p) => p[0] + p[1] === target && p[0] !== p[1])
 console.log(`Pair = ${pair}`)
-const result = pair[0] * pair[1]
-console.log(`Result = ${result}`)
+const result: number = pair[0] * pair[1]
+console.log(`Part 1 result = ${result}`)
+
+const triplets = pairs.flatMap((p) => ints.map((q) => p.concat(q)))
+const triplet = triplets.find(
+  (t) => t[0] + t[1] + t[2] === target && t[0] !== t[1] && t[1] !== t[2]
+)
+console.log(`Triplet = ${triplet}`)
+const result2: number = triplet[0] * triplet[1] * triplet[2]
+console.log(`Part 2 result = ${result2}`)
